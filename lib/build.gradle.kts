5f79391081509b0c4943406c236c3e1adc92a691
Eliza Velasquez <4576666+elizagamedev@users.noreply.github.com>
2023-03-25 14:52:58 -0700

    add ktlint, clean up build files

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,5 +1,3 @@
 plugins { id("sh.eliza.monocleimage.kotlin-library-conventions") }
 
-repositories { mavenCentral() }
-
 dependencies { implementation("io.github.markrileybot:heatshrink-java:1.0.1") }
